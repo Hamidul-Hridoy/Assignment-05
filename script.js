c9254bd7c165ed9b6551079a2440ed1c03d5a702
Hamidul-Hridoy <hamidulhridoy05@gmail.com>
2025-09-07 20:14:16 +0600

    seventh

--- script.js
+++ script.js
@@ -25,4 +25,37 @@ document.addEventListener("click", function(e) {
         alert(`Copied: ${number}`);
         copyCount++;
         updateDisplay();
-    }});
+    }
+
+    if (e.target.closest('.callBtn')) {
+        if (coinCount < 20) {
+            alert("You do not have enough coins to make a call.");
+            return;
+        } 
+        const card = e.target.closest(".card");
+        const number = card.querySelector(".number").innerText;
+        const name = card.querySelector(".name").innerText;
+        alert(`Calling ${name} : ${number}`);
+        coinCount = coinCount - 20;
+        updateDisplay();
+        const time = new Date().toLocaleTimeString();
+        const historyContainer = getElement("container");
+        const history = document.createElement('div');
+        history.innerHTML = `
+             <div class="bg-gray-100 flex items-center justify-between p-3 rounded-lg mb-2">
+                 <div>
+                     <p>${name}</p>
+                     <p class="text-gray-600">${number}</p>
+                 </div>
+                 <p>${time}</p>
+             </div>
+         `;
+        historyContainer.prepend(history);
+        updateDisplay();
+    }
+
+    if (e.target.closest(".clear")) {
+        const historyContainer = getElement('container');
+        historyContainer.innerHTML = '';
+    }
+});
